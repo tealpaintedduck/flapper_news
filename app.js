c2a6b6629c873897d96478363022ab9c946690aa
Sarah Middleton <middleton.sarahlouise@gmail.com>
2015-10-28 16:17:10 +0000

    add ability to choose post title

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ angular.module('flapperNews', [])
     ];
 
   $scope.addPost = function(){
-    $scope.posts.push({title: 'A new post!', upvotes: 0});
+    if(!$scope.title || $scope.title === "") {
+      return;
+    }
+    $scope.posts.push({title: $scope.title, upvotes: 0});
+    $scope.title = "";
   };
 
 }]);
