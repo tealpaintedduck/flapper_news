7c197f58c6a7aa9921c02dc7e894084b71070835
Sarah Middleton <middleton.sarahlouise@gmail.com>
2015-10-28 15:58:26 +0000

    posts appear on page

--- app.js
+++ app.js
@@ -3,4 +3,13 @@ angular.module('flapperNews', [])
   '$scope',
   function($scope){
     $scope.test = 'Hello world!';
-  }]);
+
+$scope.posts = [
+  'post 1',
+  'post 2',
+  'post 3',
+  'post 4',
+  'post 5'
+  ];
+
+}]);
