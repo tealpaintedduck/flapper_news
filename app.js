5aa293664bd121248ec5611ace6be1b13c024dd3
Sarah Middleton <middleton.sarahlouise@gmail.com>
2015-10-28 16:19:30 +0000

    add ability to upvote

--- app.js
+++ app.js
@@ -12,12 +12,16 @@ angular.module('flapperNews', [])
     {title: 'post 5', upvotes: 4}
     ];
 
-  $scope.addPost = function(){
+  $scope.addPost = function() {
     if(!$scope.title || $scope.title === "") {
       return;
     }
     $scope.posts.push({title: $scope.title, upvotes: 0});
     $scope.title = "";
+  };
+
+  $scope.incrementUpvotes = function(post) {
+    post.upvotes +=1;
   };
 
 }]);
